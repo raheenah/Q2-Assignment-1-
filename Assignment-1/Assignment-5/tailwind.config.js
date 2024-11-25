969820d0426fcbe986acebd95068cfda1efbb57e
Mohammed Raheenat Sunmisola <raheenah500@gmail.com>
2024-11-25 23:57:36 +0100

    Ass 5 done

--- Assignment-1/Assignment-5/tailwind.config.js
+++ Assignment-1/Assignment-5/tailwind.config.js
@@ -1,12 +1,18 @@
-
 module.exports = {
-  content: [
-    "./index.html", 
-    "./src/**/*.{html,js}",
-  ],
+  content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        input_background: "#EDEFF2",
+        input_text: "#646e7a",
+        darker_blue: "#1179BF",
+        lighter_blue: "#b9f5ff",
+        button_background: "#038e7d",
+        greyBorder: "#E0E6EA",
+        numberBorder: "#548BC5",
+        stripeBG: "#566473",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
-
